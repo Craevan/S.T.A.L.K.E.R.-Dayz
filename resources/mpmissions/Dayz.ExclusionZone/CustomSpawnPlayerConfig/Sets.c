cc05ee1863bf9cd2f9accb7f8300a7c6815ade10
Crevan <a.siverskii@gmail.com>
2023-09-20 23:32:40 +0300

    [update]: coordinates for default spawn; new admin set

--- resources/mpmissions/Dayz.ExclusionZone/CustomSpawnPlayerConfig/Sets.c
+++ resources/mpmissions/Dayz.ExclusionZone/CustomSpawnPlayerConfig/Sets.c
@@ -11,11 +11,11 @@ class StartSetsPlayersConfig
 			case 1: //Set with number 1 (Сет с номером 1)
 			{
 				My_Custom_Spawn_Parameters.RemoveAllItems(player); // Чтобы удалить с персонажа уже имеющиеся стандартные стартовые шмотки раскомментировать строку
-				itemCreated = player.GetInventory().CreateInInventory("aod_backpack_loner1_tortilla"); 	// Выдаем рюкзак и записываем в переменную itemCreated
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_BackPack"); 	// Выдаем рюкзак и записываем в переменную itemCreated
 				if (itemCreated)															// Проверяем, создался ли рюкзак. Если он создался, переменная itemCreated будет не пуста и проверка пройдет
 				{
 //					SetRandomHealthItem(itemCreated);												// Выдаем рюкзаку рандомное качество
-					itemCreated1 = itemCreated.GetInventory().CreateInInventory("TacticalBaconCan");			// Добавляем в инвентарь созданного рюкзака яблоко и записываем в переменную itemCreated1
+                    itemCreated1 = itemCreated.GetInventory().CreateInInventory("TacticalBaconCan");			// Добавляем в инвентарь созданного рюкзака консервы и записываем в переменную itemCreated1
 //					if (itemCreated1)																// Проверяем, создалось ли яблоко в рюкзаке, яблоко у нас с переменной itemCreated1
 //					{
 //						SetRandomHealthItem(itemCreated1);												// Выдаем яблоку рандомное качество
@@ -31,25 +31,37 @@ class StartSetsPlayersConfig
 //					}
 				}
 				// переменную itemCreated не обнуляем далее, поскольку мы ее не используем в проверке ниже.
-				itemCreated = player.GetInventory().CreateInInventory("aod_pants_loner5_gorka");		// Выдаем игроку штаны
-				itemCreated = player.GetInventory().CreateInInventory("aod_body_loner6_gorka");	// Выдаем игроку куртку
-				itemCreated = player.GetInventory().CreateInInventory("TacticalGloves_Black");	// Выдаем игроку перчатки
-				itemCreated = player.GetInventory().CreateInInventory("RadarCap_Brown");	// Выдаем игроку Шапку
-				itemCreated = player.GetInventory().CreateInInventory("MilitaryBoots_Brown");	// Выдаем игроку обувь
-				//itemCasted = ItemBase.Cast(itemCreated);										// Строка не нужна, закоментирована. используется для изменения класса EntityAI в ItemBase (чтобы нужные операции были доступны)
-				itemCreated = player.GetInventory().CreateInInventory("CombatKnife");		// Выдаем игроку ножик в любой свободный слот в инвентаре
-				itemCreated = player.GetInventory().CreateInInventory("Ammo_12gaPellets");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
-				itemCreated = player.GetInventory().CreateInInventory("Ammo_12gaPellets");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
-				itemCreated = player.GetInventory().CreateInInventory("Ammo_12gaPellets");		// Выдаем игроку магазины к AKM в любой свободный слот в инвентаре
-				itemCreated = NULL;															// Обнуляем значение переменной после работы с ней, нужно если мы будем использовать ее далее
-				itemCreated = player.GetHumanInventory().CreateInHands("Mp133Shotgun");				// Выдаем игроку AKM в руки
-//				if (itemCreated)															// Проверяем, создался ли АКМ
-//				{
-//					itemCreated.GetInventory().CreateAttachment( "PSO11Optic" ); 				// Выдаем игроку на AKM оптику ПСО 11 и крепим
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Pants");		// Выдаем игроку штаны
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Jacket");	// Выдаем игроку куртку
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Vest");		// Выдаем игроку жилет
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Pouches");	// Выдаем игроку подсумки к жилету
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Belt");	// Выдаем игроку ремень
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Belt_Pouches");	// Выдаем игроку подсумки к ремню
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Stalker_Sunrise_Belt_Pouches");	// Выдаем игроку подсумки к ремню
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Loner_Hat_Green");	// Выдаем игроку шапку
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Loner_Mask");	// Выдаем игроку маску
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Sunrise_Patch");	// Выдаем игроку шеврон
+                itemCreated = player.GetInventory().CreateInInventory("SGE_Sunrise_Patch");	// Выдаем игроку шеврон
+                itemCreated = player.GetInventory().CreateInInventory("NylonKnifeSheath");	// Выдаем игроку ножны
+                itemCreated = player.GetInventory().CreateInInventory("TacticalGloves_Black");	// Выдаем игроку перчатки
+                itemCreated = player.GetInventory().CreateInInventory("MilitaryBoots_Brown");	// Выдаем игроку обувь
+                //itemCasted = ItemBase.Cast(itemCreated);										// Строка не нужна, закоментирована. используется для изменения класса EntityAI в ItemBase (чтобы нужные операции были доступны)
+                itemCreated = player.GetInventory().CreateInInventory("CombatKnife");		// Выдаем игроку ножик в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Battery9V");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Mag_AoD_VSS_10Rnd");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Mag_AoD_VSS_10Rnd");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Ammo_9x39AP");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Ammo_9x39AP");	// Выдаем игроку магазины к FNX в любой свободный слот в инвентаре
+                itemCreated = player.GetInventory().CreateInInventory("Ammo_9x39AP");		// Выдаем игроку магазины к AKM в любой свободный слот в инвентаре
+                itemCreated = NULL;															// Обнуляем значение переменной после работы с ней, нужно если мы будем использовать ее далее
+                itemCreated = player.GetHumanInventory().CreateInHands("VSS_AOD");				// Выдаем игроку ВСС в руки
+                if (itemCreated)															// Проверяем, создался ли АКМ
+                {
+                    itemCreated.GetInventory().CreateAttachment( "PSO11Optic" ); 				// Выдаем игроку на AKM оптику ПСО 11 и крепим
 //					itemCreated.GetInventory().CreateAttachment( "AK_WoodBttstck" ); 			// Выдаем игроку на AKM цевье, приклад и глушитель, и крепим
 //					itemCreated.GetInventory().CreateAttachment( "AK_WoodHndgrd" );				// Выдаем игроку на AKM цевье, приклад и глушитель, и крепим
 //					itemCreated.GetInventory().CreateAttachment( "AK_Suppressor" );				// Выдаем игроку на AKM цевье, приклад и глушитель, и крепим
-//				}
+                }
 				break;
 			}
 
@@ -119,6 +131,11 @@ class StartSetsPlayersConfig
         itemCreated = player.GetInventory().CreateInInventory("BandageDressing");		// Выдаем игроку магазины к AKM в любой свободный слот в инвентаре
         itemCreated = NULL;															// Обнуляем значение переменной после работы с ней, нужно если мы будем использовать ее далее
         itemCreated = player.GetHumanInventory().CreateInHands("Toz66Obrez_AoD");				// Выдаем игроку AKM в руки
+
+        private array <vector>  spawnpoints = {"4692 54.6 1068",
+                                               "4689 54.6 1062"};
+        private vector selected_spawnpoint = spawnpoints.GetRandomElement();
+        player.SetPosition(selected_spawnpoint);
 
 
 		// ниже пример, если вам нужно задать свои точки спавна для всех игроков. Этот код можно также использовать и для каждого сета индивидуально!
